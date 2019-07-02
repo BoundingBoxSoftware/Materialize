e5b8283971cb4612fcf48b4f24db430feaa5bb55
Mtchell McCollum <mitchelldmccollum@gmail.com>
2019-07-01 20:27:33 -0400

    Finalized conversion to unity UI

--- Assets/SetPropertyMap.cs
+++ Assets/SetPropertyMap.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class SetPropertyMap : MonoBehaviour
 {
-    public string DropDownType;   
+    public string DropDownType;
+
     public void SetPropertyMapSelection(int selection)
     {
         MainGui.Instance.MapSelection(selection, DropDownType);
+    }
+
+    public void SetPreset(int selection)
+    {
+        switch (selection)
+        {
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            default:
+                break;
+        }
+    }
+    private void setChannels(int red, int blue, int green, int alpha)
+    {
+
     }
 }
