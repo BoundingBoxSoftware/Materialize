2fb76478e014747ca6553e9657e6b32a84ef7dd5
Mtchell McCollum <mitchelldmccollum@gmail.com>
2019-07-06 22:35:47 -0400

    Upgraded to 2019.1.9f1 and added presets

--- Assets/SetPropertyMap.cs
+++ Assets/SetPropertyMap.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetPropertyMap : MonoBehaviour
 {
     public string DropDownType;
+    public Dropdown RedMap;
+    public Dropdown BlueMap;
+    public Dropdown GreenMap;
+    public Dropdown AlphaMap;
+
 
     public void SetPropertyMapSelection(int selection)
     {
@@ -19,10 +25,10 @@ public class SetPropertyMap : MonoBehaviour
 
                 break;
             case 1:
-
+                setChannels(1, 0, 4, 2);
                 break;
             case 2:
-
+                setChannels(2, 4, 1, 0);
                 break;
             default:
                 break;
@@ -30,6 +36,15 @@ public class SetPropertyMap : MonoBehaviour
     }
     private void setChannels(int red, int blue, int green, int alpha)
     {
+
+        RedMap.value = red;
+        BlueMap.value = blue;
+        GreenMap.value = green;
+        AlphaMap.value = alpha;
+        //RedMap.SetPropertyMapSelection(red);
+        //BlueMap.SetPropertyMapSelection(blue);
+        //GreenMap.SetPropertyMapSelection(green);
+        //AlphaMap.SetPropertyMapSelection(alpha);
 
     }
 }
